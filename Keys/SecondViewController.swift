94f51cb1040e1bc7d647c186db73affdb44daebc
Sorin Lumezeanu <slumezeanu@yahoo.com>
2016-03-02 21:46:22 +0200

    Initial Commit

---
+++ Keys/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Keys
+//
+//  Created by Sorin Lumezeanu on 3/2/16.
+//  Copyright © 2016 Sorin Lumezeanu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
